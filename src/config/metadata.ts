2a63d40f2e837ae0a12219e10dfdde27ef0b945b
GoodbyeMyself <15500409521@163.com>
2025-07-24 17:42:41 +0800

    Copyright  剔除

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -1,14 +1,13 @@
 const metadata = {
-  title: 'OpenByteCode',
-  copyright: `Copyright © ${new Date().getFullYear()} OpenByteCode`,
-  website: 'http://www.openbytecode.com/',
-  description: '数据血缘，大数据，Open-Lineage，OpenByteCode，Antv',
-  github: 'https://github.com/lijunping365/Open-Lineage-Web',
-  juejin: 'https://juejin.cn/user/3465313255425208',
-  zhihu: 'https://www.zhihu.com/people/jun-jun-1-31-24',
-  bilibili: 'https://space.bilibili.com/383654866',
-  theme: 'system', // system, dark or light
-  navLinks: [{ href: 'https://openbytecode.com/', title: 'OpenByteCode' }],
+    title: 'OpenByteCode',
+    website: 'http://www.openbytecode.com/',
+    description: '数据血缘，大数据，Open-Lineage，OpenByteCode，Antv',
+    github: 'https://github.com/lijunping365/Open-Lineage-Web',
+    juejin: 'https://juejin.cn/user/3465313255425208',
+    zhihu: 'https://www.zhihu.com/people/jun-jun-1-31-24',
+    bilibili: 'https://space.bilibili.com/383654866',
+    theme: 'system', // system, dark or light
+    navLinks: [{ href: 'https://openbytecode.com/', title: 'OpenByteCode' }],
 };
 
 export default metadata;
