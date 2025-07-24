9d67a5b663a25f1553665be1db8a465d5a2c6d25
GoodbyeMyself <15500409521@163.com>
2025-07-24 17:40:06 +0800

    去掉 Eslint 校验

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // 获取 .env 环境配置文件
-  const { VITE_PUBLIC_PATH } = loadEnv(mode, process.cwd());
+    // 获取 .env 环境配置文件
+    const { VITE_PUBLIC_PATH } = loadEnv(mode, process.cwd());
 
-  return {
-    base: VITE_PUBLIC_PATH,
-    plugins: [react(), eslint()],
-  };
+    return {
+        base: VITE_PUBLIC_PATH,
+        plugins: [react()],
+    };
 });
